8552125f99cfcd43f8501158746c5b9f3a902fa6
deeksha <duddusmail@gmail.com>
2024-08-10 12:54:07 +0530

    changed footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ export default function Footer() {
 
       <footer className="bg-white rounded-lg shadow dark:bg-gray-800">
           <div className="w-full  max-w-screen-xl mx-auto p-4 md:flex md:items-center md:justify-between">
-            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <Link to="https://flowbite.com/" className="hover:underline">Cinematic Spark</Link>. All Rights Reserved.
+            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <Link to="https://flowbite.com/" className="hover:underline">Cinematic_Spark</Link>. All Rights Reserved.
             </span>
             <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
                 <li>
